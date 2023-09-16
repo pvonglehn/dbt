9fcfa5635262803718a0e2116e1653e20f2dfaba
Patrick von Glehn <patrickvonglehn@gmail.com>
2023-09-16 14:43:48 +0000

    add daily and weekly alc

--- models/consumption/alcohol_aggregated.sql
+++ models/consumption/alcohol_aggregated.sql
@@ -1,26 +1,50 @@
 WITH daily AS (
-select
-    date_trunc(date_time, day) as dday
-  , 'daily' AS aggregation
-  , COUNT(*) AS drinks
-from {{ ref("alcohol") }}
-where date_trunc(date_time, week) > '2022-07-01'
-group by 1
-order by 1 asc)
+  SELECT
+    date_trunc(date_time, day) AS dday,
+    'daily' AS aggregation,
+    COUNT(*) AS drinks
+  FROM {{ ref("alcohol") }}
+  WHERE date_trunc(date_time, week) > '2022-07-01'
+  GROUP BY 1
+)
+
+, date_range AS (
+  SELECT
+    DATE_ADD(DATE '2022-07-01', INTERVAL seq.day DAY) AS dday
+  FROM (
+    SELECT
+      ROW_NUMBER() OVER() AS day
+    FROM
+      UNNEST(GENERATE_ARRAY(0, DATE_DIFF(CURRENT_DATE(), DATE '2022-07-01', DAY))) AS seq
+  ) AS seq
+)
+
+, daily_all_days AS (SELECT
+  d.dday,
+  'daily' AS aggregation,
+  IFNULL(drinks, 0) AS drinks
+FROM
+  date_range AS d
+LEFT JOIN
+  daily AS a
+ON
+  d.dday = DATE(a.dday)  -- Cast a.dday to DATE
+ORDER BY
+  dday ASC)
 
 , weekly AS (
 select
-    date_trunc(date_time, week) as dday
+    date_trunc(dday, week) as dday
   , 'weekly' AS aggregation
-  , COUNT(*) AS drinks
-from {{ ref("alcohol") }}
-where date_trunc(date_time, week) > '2022-07-01'
+  , SUM(drinks) AS drinks
+from daily_all_days
+where date_trunc(dday, week) > '2022-07-01'
 group by 1
 order by 1 asc    
 )
 
 SELECT *
-FROM daily
+FROM daily_all_days
 UNION ALL 
 SELECT *
 FROM weekly
